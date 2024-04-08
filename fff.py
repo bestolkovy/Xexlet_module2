26098baa9ee86ad1700dc7d3e29d96b45601ae5d
vito <vbykov@inbox.ru>
2024-04-08 18:00:41 +0700

    clear

--- fff.py
+++
@@ -1,9 +0,0 @@
-def count_all(items):
-    counters = {}
-    for item in items:
-        print (item)
-        counters[item] = counters.get(item, 0) + 1
-        print(counters)
-    return counters
-
-count_all('heelohhhhh')
