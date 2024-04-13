5977909f377a5daad39bb88c04dde65085e5cdea
vito <vbykov@inbox.ru>
2024-04-13 16:30:08 +0700

    lesson8

--- lesson8.py
+++ lesson8.py
@@ -1,4 +1,3 @@
 def apply_diff(target, diff):
-    target.update(diff.get['add'])
-    target.difference_update(diff.get['remove'])
-
+    target.update(diff.get('add', {}))
+    target.difference_update(diff.get('remove', {}))
