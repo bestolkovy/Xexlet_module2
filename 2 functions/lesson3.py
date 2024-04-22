4fa3904199c78d90fed0ea774fc878da538cfd3c
bestolkovy <f326ehm@gmail.com>
2024-04-22 21:08:18 +0700

    lesson3 of 2th added

---
+++ 2 functions/lesson3.py
@@ -0,0 +1,12 @@
+# выдает ргб цвета по запросу цвета.
+
+def rgb(red=0, green=0, blue=0):
+    return f'rgb({red}, {green}, {blue})'
+
+
+def get_colors():
+    return {
+                'red': rgb(red=255),
+                'blue': rgb(blue=255),
+                'green': rgb(green=255)
+    }
