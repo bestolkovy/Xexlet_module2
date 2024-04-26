1a198ffb5445a76890aa7ec9e087a244da17016b
vito <vbykov@inbox.ru>
2024-04-26 23:05:37 +0700

    lesson5 added

---
+++ lesson5.py
@@ -0,0 +1,12 @@
+def get_first_name(name_surname):
+    first_name = ''
+    for char in name_surname:
+        if char != '_':
+            first_name = first_name + char
+        else:
+            return first_name
+
+
+def sort_by(key_func, user_list):
+    sorted_list = sorted(user_list, key=key_func)
+    return sorted_list
